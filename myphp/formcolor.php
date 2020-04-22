9454e0549d3eead5aa9c04c31d09559f833fb307
Naiaos84 <naiaos84@gmail.com>
2020-04-22 11:57:00 +0700

    22/4/63

---
+++ myphp/formcolor.php
@@ -0,0 +1,47 @@
+<doctype html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv="x-UA-Compatidle" content="IE=edge">
+    <tithe> method post </tithe>
+    <like rel="stylesheet" href="">
+</head>
+
+    <h1>form register</h1>
+    <form action="formadio.php" method="post">
+        username : 
+        <input type="text" name="ชื่อของคุณ" required>
+        password : 
+        <input type="text" name="รหัสของคุณ" required>
+        <br> <br>
+        name : 
+        <input type="text" name="ชื่อจริงของคุณ" required>
+        phone : 
+        <input type="text" name="เบอร์ของคุณ" required>
+        <br> <br>
+        email : 
+        <input type="email" name="emailของคุณ" required>
+        <form action="get">
+            เลือกวันทีสิ 
+            <input type="date" name="mydete">
+            <br> <br>
+            my numder :
+            <input thpe="numder" name="เลขของคุณ">
+            
+            เลือกสีที่ชอบสิ :
+            <input type="color" name="mycolor">
+            <br>
+            <button type="submit">save</button>
+        </form>
+
+</body>
+</html>
+
+</body>
+</html>
+
+    </form>
+</body>
+</html>
+        
+ 
