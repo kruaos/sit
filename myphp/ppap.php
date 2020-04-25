092daed86e48927563315f3f125fb54be5ad086f
Naiaos84 <naiaos84@gmail.com>
2020-04-25 11:52:12 +0700

    15/4/2563

---
+++ myphp/ppap.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="frog.php" href="">
+</head>
+<body>
+
+<h1> hello jonny </h1>
+    <form action="frog.php" methot="post">
+        username
+        <input type="text" name="hello jonny" placeholder="
+        username" required>
+
+        <button type="submit">save</button>
+    </form>
+</body>
+</html>
