f265e45bac22af73226bff568a89910de1170a76
Naiaos84 <naiaos84@gmail.com>
2020-04-26 11:34:00 +0700

    26/4/2563

---
+++ myphp/lemon.php
@@ -0,0 +1,61 @@
+<doctype html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv="x-UA-Compatidle" content="IE=edge">
+    <tithe> method post </tithe>
+    <like rel="stylesheet" href="">
+
+</head>
+<body>
+    <?php
+        //ไอดีที่เเก้ไข
+        $id = $_get['id'];
+
+        echo $id;
+
+    ?>
+    <img src="thai.png" width="1000px" height="100px">
+    <h1>form register</h1>
+    <form action="formadio.php" method="post">
+        username : 
+        <input type="text" name="ชื่อของคุณ" required>
+        password : 
+        <input type="text" name="รหัสของคุณ" required>
+        <br> <br>
+        name : 
+        <input type="text" name="ชื่อจริงของคุณ" required>
+        phone : 
+        <input type="text" name="เบอร์ของคุณ" required>
+        <br> <br>
+        email : 
+        <input type="email" name="emailของคุณ" required>
+        <form action="get">
+            เลือกวันทีสิ 
+            <input type="date" name="mydete">
+            <br> <br>
+            my numder :
+            <input thpe="numder" name="เลขของคุณ">
+            
+            เลือกสีที่ชอบสิ :
+            <input type="color" name="mycolor">
+            <br>
+            <button type="submit">save</button>
+        </form>
+        
+        <h4>ลองเปิดดูสิXD DX LOL55</h4>
+        
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/xEpKh-A8M_Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <img src="thai.png" width="1000px" height="100px">
+</body>
+</html>
+
+</body>
+</html>
+
+    </form>
+</body>
+</html>
+        
+ 
