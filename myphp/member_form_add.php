73099be654fc3296b80aa377df21e4cc0ee5a01a
Naiaos84 <naiaos84@gmail.com>
2020-05-05 10:54:07 +0700

    5/5/2563

--- myphp/member_form_add.php
+++ myphp/member_form_add.php
@@ -7,28 +7,45 @@
     <like rel="stylesheet" href="">
 </head>
 <body>
+    <?php
+        include 'condb.php';
+        //ไอดีที่ส่งมาแก้ไข
+        $id = $_GET['id'];
+        //echo $id;
+        $query ="SELECT * FROM tbl_member WHERE id=$id";
+        $result = mysqli_query($condb, $query) or die("error in sql : 
+            $query". mysqli_error($query));
+        $row = mysqli_fetch_array($result);
+    
+        //print_r($row);
+    
+    
+    ?>
 
     <h1>สมาชิก/เเก้ไขปรับปรุงสมาชิก</h1>
         <form action="member_form_add_db.php" method="post">
+        id
+        <input type="number" name="id" value="<?php echo $row['id'];?>" readonly>
+        <br>
         username
         <input type="text" name="username" placeholder="username"
-        required>
+        required value="<?php echo $row['username'];?>">
         <br>
         password
         <input type="password" name="password" placeholder="password"
-        required>
+        required value="<?php echo $row['password'];?>">
         <br>
         name
         <input type="texe" name="name" placeholder="name"
-        required>
+        required value="<?php echo $row['name'];?>">
         <br>
         phone
         <input type="text" name="phone" placeholder="phone"
-        required>
+        required value="<?php echo $row['phone'];?>">
         <br>
         email
         <input type="email" name="email" placeholder="email"
-        required>
+        required value="<?php echo $row['email'];?>">
         <br>
         <button type="submit">save </button>
         </form>
--- myphp/member_form_add_db.php
+++ myphp/member_form_add_db.php
@@ -1,19 +1,19 @@
-
 <?php
 include 'condb.php';
 
 
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+ echo '<pre>';
+ print_r($_POST);
+ echo '</pre>';
 
-echo '<hr>';
+ echo '<hr>';
 
-echo '<pre>';
-var_dump($_POST);
-echo '</pre>';
+ echo '<pre>';
+ var_dump($_POST);
+ echo '</pre>';
 
 
+exit;
 
 //ประกาศตัวเเปร รับค่าจากฟอร์ม
     $username = $_POST ['username'];
@@ -21,37 +21,40 @@ echo '</pre>';
     $name = $_POST ['name'];
     $phone = $_POST ['phone'];
     $email = $_POST ['email'];
-
+    $id= $_POST['id'];
 
 //insert into table
  
-    $sql = "INSERT INTO tbl_member
-    (
-    username,
-    password,
-    name,
-    phone,
-    email
-    )
-    VALUES
-    (
-    '$username',
-    '$password',
-    '$name',
-    '$phone',
-    '$email'
-    )
+    $sql = "UPDATE tpl_member SET
+    
+    username='$username',
+    password='$password',
+    name='$name',
+    phone='$phone',
+    email='$email'
+    
+    WHERE id = $id
     ";
 
     $result = mysqli_query($condb, $sql) or die("error in sql : $sql". mysqli_error($sql)) ;
     mysqli_close($condb) ;
      
+     echo $sql;
 
+     echo '<hr>';
 
-
-    if($result) {
-        echo 'Insert Successfully';
+    if($result){
+        echo "<script type='text/javascript'>";
+            echo "alert('Insert Successfully');";
+            echo "window. location = 'member_list.php'; ";
+        echo "</script>";
     }else{
-        echo 'Error !!';
+        echo "<script type='text/javascript'>";
+            echo "alert('error !!');";
+            echo "window. location = 'member_list.php'; ";
+        echo "</script>";
     }
+
+
+
     ?>
