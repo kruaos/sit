d945f979f9d7c4fd6a0f84a07188d00291ae1eae
Naiaos84 <naiaos84@gmail.com>
2020-06-08 15:51:14 +0700

    8/6/2563

---
+++ ci/application/controllers/Mypage.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Hello extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('hello_views');
+    }
+    public function index2()
+	{
+		$this->load->view('index2_view');
+	}
+}
+ 
