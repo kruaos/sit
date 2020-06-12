031bffe5f53b679114e925d0bca2b1ae043e9ca6
Naiaos84 <naiaos84@gmail.com>
2020-06-12 14:50:10 +0700

    12/6/2563

--- ci/application/views/Hello_views.php
+++ ci/application/views/Hello_views.php
@@ -8,6 +8,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 	<h1> view index2</h1>
-	
+
+	<a href="https://www.youtube.com/watch?v=XEEasR7hVhA&list=RDiywaBOMvYLI&index=2"> link to Slipknot - The Devil in l </a>
+	<br> <br> <br><br>
+	<a href=http://127.0.0.1:5500/html-pa/index20.html> รับสมัครคำรักชาติ </a>
+	<br> <br> <br><br>
+	<img src="<?php echo base_url('img');?>/seal.jpg" alt="">
 </body>
 </html>
