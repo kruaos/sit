654fd23ceeb9263f812b4e5a271c4ca1986c4e02
Naiaos84 <naiaos84@gmail.com>
2020-06-15 15:31:38 +0700

    15/6/2563

--- ci/application/controllers/hello.php
+++ ci/application/controllers/hello.php
@@ -5,7 +5,9 @@ class Hello extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('mycss');
 		$this->load->view('hello_views');
+		$this->load->view('myjs');
 	}
 	public function index2()
 	{
