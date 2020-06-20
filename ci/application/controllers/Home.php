8443299641c774c266856dbd18a81b69a55e1e93
Naiaos84 <naiaos84@gmail.com>
2020-06-20 15:51:55 +0700

    20/6/2563

--- ci/application/controllers/Home.php
+++ ci/application/controllers/Home.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
     {
+        $this->load->view('css');
+        $this->load->view('header');
+        $this->load->view('banner');
         $this->load->view('home_view');
+        $this->load->view('footer');
+        $this->load->view('js');
     }
 }
